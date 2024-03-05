6fa7ba923e6aab6cead722a3060e3efd806baf26
Sonava <sonava@code.edu.az>
2024-03-06 03:54:01 +0400

    Initial Commit

---
+++ KarmaApp/Controllers/PagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KarmaApp.Controllers
+{
+	public class PagesController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+	}
+}
---
+++ KarmaApp/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KarmaApp.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Details()
+		{
+			return View();
+		}
+	}
+}
